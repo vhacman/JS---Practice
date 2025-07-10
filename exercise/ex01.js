6dce22b98f2687168ddf9957f08bea490af9729e
vhacman <hacmanvioricagabriela@gmail.com>
2025-07-10 17:16:25 +0200

    exercises

---
+++ exercise/ex01.js
@@ -0,0 +1,73 @@
+/*
+Esercizio 1
+Dichiarare una variabile chiamata "nome" e assegnarle il tuo nome.
+ Stampare la variabile sulla console.*/
+var Nome = "Gabriela";
+console.log(Nome);
+
+/*
+Esercizio 2
+Dichiarare una variabile chiamata "età" e assegnarle il tuo numero di anni. Stampare la
+variabile sulla console.*/ 
+var age = 26;
+console.log(age);
+
+/*
+Esercizio 3
+Dichiarare una variabile chiamata "verificato" e assegnarle il valore booleano true. 
+Stampare la variabile sulla console.*/
+var verificato = true;
+console.log(verificato);
+
+/*
+Esercizio 4
+Dichiarare una variabile chiamata "numeri" e assegnarle un array
+ contenente una serie di numeri. Stampare l'array sulla console.*/
+ var nums = [1, 2, 3, 4, 5];
+ console.log(nums);
+
+ /*
+Esercizio 5
+Dichiarare una variabile chiamata "persona" e assegnarle un oggetto con le seguenti proprietà: 
+"nome" (con il tuo nome) e "età" (con il tuo numero di anni). Stampare l'oggetto sulla console.*/
+let Persona = {
+    nome: "Gabriela",
+    age: 26,
+};
+
+console.log(Persona);
+/*
+Esercizio 6
+Dichiarare una variabile chiamata "nullo" e assegnarle il valore null. 
+Stampare la variabile sulla console.*/
+var nullo = null;
+
+console.log(nullo);
+
+/*
+Esercizio 7
+Dichiarare una variabile chiamata "nonDefinito" senza assegnarle un valore.
+ Stampare la variabile sulla console.*/
+var nonDefinito;
+
+console.log(nonDefinito);
+
+ /*
+Esercizio 8
+Eseguire un'operazione matematica non valida, ad esempio dividere una stringa per un numero.
+ Stampare il risultato sulla console.*/
+
+var string = 'string';
+var num = 4;
+
+var result = string + num;
+console.log(result);
+
+
+ /*
+Esercizio 9
+Dichiarare una variabile chiamata "simbolo" e assegnarle un valore di tipo Symbol. 
+Stampare la variabile sulla console.*/
+
+var simbolo = Symbol();
+console.log(simbolo);
---
+++ exercise/ex_00.js
@@ -0,0 +1,57 @@
+/* 1.
+Dichiarare una variabile chiamata "numero" 
+e assegnarle il valore 5. Stampare il valore della variabile sulla console.
+*/
+
+let numero = 5;
+console.log(numero);
+
+
+/*
+2.
+Dichiarare una costante chiamata "PI"
+ e assegnarle il valore di 3.14. Stampare il valore della costante sulla consol*/
+
+const   PI = 3.14;
+console.log(PI);
+
+/*3.
+Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". 
+Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
+*/
+let nome = 'marco';
+nome = 'Luca';
+console.log(nome);
+
+/*4.
+Dichiarare una variabile chiamata "saluto"
+ e assegnarle il valore "Ciao, come stai?". Stampare il valore della variabile sulla console.*/ 
+
+var saluto = "Ciao, come stai?";
+console.log(saluto);
+
+/*5.
+Dichiarare una variabile chiamata "vero" e assegnarle 
+il valore true. Stampare il valore della variabile sulla console.*/
+
+var vero = true;
+console.log(vero);
+
+/*6.
+Dichiarare una variabile chiamata "numeri" e 
+assegnarle un array contenente i numeri da 1 a 5. Stampare l'array sulla console.*/
+
+var numeri = [1, 2, 3, 4];
+console.log(numeri);
+
+
+/*7. 
+Dichiarare una variabile chiamata "persona" e assegnarle un oggetto con le seguenti proprietà: 
+"nome" (con valore "Mario") e "età" (con valore 30). Stampare l'oggetto sulla console.*/
+
+let persona = {
+    nome: "Mario",
+    eta: 30,    
+};
+
+console.log(persona);
