192c2e90b1b0b943ed4f8f5d5152bbe73fae9ca4
vhacman <hacmanvioricagabriela@gmail.com>
2025-07-22 17:16:42 +0200

    another little project

---
+++ exercise/sentence_maker/script.js
@@ -0,0 +1,77 @@
+
+
+// ______               ______          __     ______                      ____               _           __ 
+// / ____/_______  ___  / ____/___  ____/ /__  / ____/___ _____ ___  ____  / __ \_________    (_)__  _____/ /_
+// / /_  / ___/ _ \/ _ \/ /   / __ \/ __  / _ \/ /   / __ `/ __ `__ \/ __ \/ /_/ / ___/ __ \  / / _ \/ ___/ __/
+// / __/ / /  /  __/  __/ /___/ /_/ / /_/ /  __/ /___/ /_/ / / / / / / /_/ / ____/ /  / /_/ / / /  __/ /__/ /_  
+// /_/   /_/   \___/\___/\____/\____/\__,_/\___/\____/\__,_/_/ /_/ /_/ .___/_/   /_/   \____/_/ /\___/\___/\__/  
+//                                                               /_/                    /___/                 
+
+// ▗▄▄▖▗▄▄▄▖▗▖  ▗▖▗▄▄▄▖▗▄▄▄▖▗▖  ▗▖ ▗▄▄▖▗▄▄▄▖    ▗▖  ▗▖ ▗▄▖ ▗▖ ▗▖▗▄▄▄▖▗▄▄▖ 
+// ▐▌   ▐▌   ▐▛▚▖▐▌  █  ▐▌   ▐▛▚▖▐▌▐▌   ▐▌       ▐▛▚▞▜▌▐▌ ▐▌▐▌▗▞▘▐▌   ▐▌ ▐▌
+//  ▝▀▚▖▐▛▀▀▘▐▌ ▝▜▌  █  ▐▛▀▀▘▐▌ ▝▜▌▐▌   ▐▛▀▀▘    ▐▌  ▐▌▐▛▀▜▌▐▛▚▖ ▐▛▀▀▘▐▛▀▚▖
+// ▗▄▄▞▘▐▙▄▄▖▐▌  ▐▌  █  ▐▙▄▄▖▐▌  ▐▌▝▚▄▄▖▐▙▄▄▖    ▐▌  ▐▌▐▌ ▐▌▐▌ ▐▌▐▙▄▄▖▐▌ ▐▌
+                                                                        
+                                                                        
+                                                                        
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+// Prima storia
+adjective  = "active";
+noun       = "Dragon";
+verb       = "hungry";
+place      = "forest";
+adjective2 = "slim";
+noun2      = "apples";
+
+let firstStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("First story: " + firstStory);
+
+// Seconda storia
+adjective  = "brave";
+noun       = "knight";
+verb       = "battling";
+place      = "enchanted castle";
+adjective2 = "gleaming";
+noun2      = "dragons";
+
+let secondStory =
+  "Once upon a time, there was a(n) " +
+  adjective +
+  " " +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  ". The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+
+console.log("Second story: " + secondStory);
+s
