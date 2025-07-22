721b696e29f6098a59a3e7e3535e0567d57b7f72
vhacman <hacmanvioricagabriela@gmail.com>
2025-07-22 16:50:35 +0200

    triviabot

---
+++ TriviaBot/script.js
@@ -0,0 +1,59 @@
+
+
+
+// ▗▄▄▄▖▗▄▄▖ ▗▄▄▄▖▗▄▄▄▖ ▗▄▄▖ ▗▄▖ ▗▄▄▄ ▗▄▄▄▖ ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▖ ▗▄▄▖ ▗▄▄▖  ▗▄▖    ▗▖▗▄▄▄▖ ▗▄▄▖▗▄▄▄▖
+// ▐▌   ▐▌ ▐▌▐▌   ▐▌   ▐▌   ▐▌ ▐▌▐▌  █▐▌   ▐▌   ▐▌ ▐▌▐▛▚▞▜▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌   ▐▌▐▌   ▐▌     █  
+// ▐▛▀▀▘▐▛▀▚▖▐▛▀▀▘▐▛▀▀▘▐▌   ▐▌ ▐▌▐▌  █▐▛▀▀▘▐▌   ▐▛▀▜▌▐▌  ▐▌▐▛▀▘ ▐▛▀▘ ▐▛▀▚▖▐▌ ▐▌   ▐▌▐▛▀▀▘▐▌     █  
+// ▐▌   ▐▌ ▐▌▐▙▄▄▖▐▙▄▄▖▝▚▄▄▖▝▚▄▞▘▐▙▄▄▀▐▙▄▄▖▝▚▄▄▖▐▌ ▐▌▐▌  ▐▌▐▌   ▐▌   ▐▌ ▐▌▝▚▄▞▘▗▄▄▞▘▐▙▄▄▖▝▚▄▄▖  █  
+                                                                                                                                                                                   
+       
+
+// ░██████████    ░██          ░██          ░████████    ░██████     ░██    
+//     ░██                                  ░██    ░██  ░██   ░██    ░██    
+//     ░██░██░████░██░██    ░██░██ ░██████  ░██    ░██ ░██     ░██░████████ 
+//     ░██░███    ░██░██    ░██░██      ░██ ░████████  ░██     ░██   ░██    
+//     ░██░██     ░██ ░██  ░██ ░██ ░███████ ░██     ░██░██     ░██   ░██    
+//     ░██░██     ░██  ░██░██  ░██░██   ░██ ░██     ░██ ░██   ░██    ░██    
+//     ░██░██     ░██   ░███   ░██ ░█████░██░█████████   ░██████      ░████ 
+                                                                         
+                                                                         
+                                                                         
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+
+botName = "FunBot";
+botLocation = "Earth";
+
+let favoriteLanguage;
+
+favoriteLanguage = "C programming";
+
+let phrase;
+
+phrase = "My name is " + botName + " and I live on " + botLocation + '.';
+
+console.log(phrase);
+
+let phrase2;
+
+phrase2 = "My favorite programming language is " + favoriteLanguage + '.';
+
+console.log(phrase2);
+
+let codingFact;
+
+codingFact = "Did you know that " + favoriteLanguage + " is the father of all programming languages. ";
+
+console.log(codingFact);
+
+codingFact = "Another cool fact about " + favoriteLanguage + ": it was created in 1972 and is still massively used today!";
+
+console.log(codingFact);
+
+codingFact = "Fun fact: many modern languages like C++ and Java share their syntax roots in " + favoriteLanguage + ".";
+
+console.log(codingFact);
+
